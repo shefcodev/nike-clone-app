0b9aea982e1de48ff8842aad09ca068744562162
Sherif Jimoh <Sherefjimoh@gmail.com>
2024-04-05 13:22:18 +0100

    rendered customers-reviews section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,14 @@ const Button = ({
   textColor,
 }) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full 
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'bg-coral-red text-white border-coral-red'
+      }`}
+    >
       {label}
       {iconURL && (
         <img
